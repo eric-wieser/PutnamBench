be87f071e1360464fdd5ff59e06f07980d2f2526
Eric Wieser <wieser.eric@gmail.com>
2024-09-25 23:50:07 +0000

    This test will fail

--- lean4/src/putnam_1962_a1.lean
+++ lean4/src/putnam_1962_a1.lean
@@ -4,6 +4,8 @@ open BigOperators
 open MeasureTheory
 
 /--
+THIS IS A TEST
+
 Given five points in a plane, no three of which lie on a straight line, show that some four of these points form the vertices of a convex quadrilateral.
 -/
 theorem putnam_1962_a1
